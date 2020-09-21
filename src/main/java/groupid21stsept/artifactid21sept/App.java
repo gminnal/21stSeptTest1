68773393a72f919b126b221e03588ca31f5a627b
Minnal <gyaniminnal@gmail.com>
2020-09-21 13:43:44 +0530

    Frst Commit

---
+++ src/main/java/groupid21stsept/artifactid21sept/App.java
@@ -0,0 +1,13 @@
+package groupid21stsept.artifactid21sept;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/groupid21stsept/artifactid21sept/AppTest1.java
@@ -0,0 +1,11 @@
+package groupid21stsept.artifactid21sept;
+
+public class AppTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
