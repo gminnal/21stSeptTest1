e8a850db06ce24d971ee85a73c9bf0e7bfc495f0
Minnal <gyaniminnal@gmail.com>
2020-09-21 13:53:43 +0530

    First Commit

---
+++ src/test/java/groupid21stsept/artifactid21sept/AppTest2.java
@@ -0,0 +1,12 @@
+package groupid21stsept.artifactid21sept;
+
+public class AppTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("AppTest2");
+		
+	}
+
+}
